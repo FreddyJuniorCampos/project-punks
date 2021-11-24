85bcf3429d8ba1fbcbc8219c8b32bec696c2bf56
Freddy Junior Campos <freddycampos1994@gmail.com>
2021-11-24 13:49:56 -0400

    Added metadata ERC721

--- contracts/FreddyPunks.sol
+++ contracts/FreddyPunks.sol
@@ -2,7 +2,68 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Base64.sol";
 
-contract FreddyPunks is ERC721 {
-  constructor() ERC721("FJNFT", "FJNFTS") {}
+contract FreddyPunks is ERC721, ERC721Enumerable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _idCounter;
+    uint256 public maxSupply;
+
+    constructor(uint256 _maxSupply) ERC721("FJNFT", "FJNFTS") {
+        maxSupply = _maxSupply;
+    }
+
+    function mint() public {
+        uint256 current = _idCounter.current();
+        require(current < maxSupply, "No FreddyPunks left");
+
+        _safeMint(msg.sender, current);
+        _idCounter.increment();
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721 Metadata: URI query for nonexistent token"
+        );
+
+        string memory jsonURI = Base64.encode(
+            abi.encodePacked(
+                '{ "name": "FreddyPunks #"}',
+                tokenId,
+                '", "description": "Freddy Punks are randomized Avatars stored on chain to teach DApp development", "image": "',
+                "// TODO: Calculate image URL",
+                '"}'
+            )
+        );
+
+        return
+            string(abi.encodePacked("data:application/json;base64,", jsonURI));
+    }
+
+    // Override required
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
