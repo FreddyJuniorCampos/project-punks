da371726e888c2f0dd3dd067c320f9d2075d94ef
Freddy Junior Campos <freddycampos1994@gmail.com>
2021-11-24 12:30:24 -0400

    Added the constructor of NFT

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const deploy = async () => {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contract with the account: ", deployer.address);
+
+  const FreddyPunks = await ethers.getContractFactory("FreddyPunks");
+  const deployed = await FreddyPunks.deploy();
+
+  console.log("Freddy Punks is deployed at:", deployed.address);
+};
+
+deploy()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
