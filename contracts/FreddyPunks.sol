14e1d4d1a8ef8e91e1dce44e5302916c81b814d1
Freddy Junior Campos <freddycampos1994@gmail.com>
2021-11-25 10:46:34 -0400

    Added image URI

--- contracts/FreddyPunks.sol
+++ contracts/FreddyPunks.sol
@@ -5,12 +5,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Base64.sol";
+import "./FreddyPunksDNA.sol";
 
-contract FreddyPunks is ERC721, ERC721Enumerable {
+contract FreddyPunks is ERC721, ERC721Enumerable, FreddyPunksDNA {
     using Counters for Counters.Counter;
 
     Counters.Counter private _idCounter;
     uint256 public maxSupply;
+    mapping(uint256 => uint256) public tokenDNA;
 
     constructor(uint256 _maxSupply) ERC721("FJNFT", "FJNFTS") {
         maxSupply = _maxSupply;
@@ -20,8 +22,57 @@ contract FreddyPunks is ERC721, ERC721Enumerable {
         uint256 current = _idCounter.current();
         require(current < maxSupply, "No FreddyPunks left");
 
+        tokenDNA[current] = deterministicPseudoRandomDNA(current, msg.sender);
         _safeMint(msg.sender, current);
         _idCounter.increment();
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://avataaars.io/";
+    }
+
+    function _paramsURI(uint256 _dna) internal view returns (string memory) {
+        string memory params;
+
+        {
+            params = string(
+                abi.encodePacked(
+                    "accessoriesType=",
+                    getAccessoriesType(_dna),
+                    "&clotheColor=",
+                    getClotheColor(_dna),
+                    "&clotheType=",
+                    getClotheType(_dna),
+                    "&eyeType=",
+                    getEyeType(_dna),
+                    "&eyebrowType=",
+                    getEyeBrowType(_dna),
+                    "&facialHairColor=",
+                    getFacialHairColor(_dna),
+                    "&facialHairType=",
+                    getFacialHairType(_dna),
+                    "&hairColor=",
+                    getHairColor(_dna),
+                    "&hatColor=",
+                    getHatColor(_dna),
+                    "&graphicType=",
+                    getGraphicType(_dna),
+                    "&mouthType=",
+                    getMouthType(_dna),
+                    "&skinColor=",
+                    getSkinColor(_dna)
+                )
+            );
+        }
+
+        return string(abi.encodePacked(params, "&topType=", getTopType(_dna)));
+    }
+
+    function imageByDNA(uint256 _dna) public view returns (string memory) {
+        string memory baseURI = _baseURI();
+        string memory paramsURI = _paramsURI(_dna);
+
+        return string(abi.encodePacked(baseURI, "?", paramsURI));
     }
 
     function tokenURI(uint256 tokenId)
@@ -35,12 +86,15 @@ contract FreddyPunks is ERC721, ERC721Enumerable {
             "ERC721 Metadata: URI query for nonexistent token"
         );
 
+        uint256 dna = tokenDNA[tokenId];
+        string memory image = imageByDNA(dna);
+
         string memory jsonURI = Base64.encode(
             abi.encodePacked(
                 '{ "name": "FreddyPunks #"}',
                 tokenId,
                 '", "description": "Freddy Punks are randomized Avatars stored on chain to teach DApp development", "image": "',
-                "// TODO: Calculate image URL",
+                image,
                 '"}'
             )
         );
